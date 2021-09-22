366de2cb6e30aaec6a75768a6a2c8a4e16f10b5a
AnaCarriel <ana.carriel096@gmail.com>
2021-09-21 21:45:06 -0300

    Ajustado o controller

--- src/main/java/br/com/anacarriel/controller/PersonController.java
+++ src/main/java/br/com/anacarriel/controller/PersonController.java
@@ -19,7 +19,7 @@ public class PersonController {
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) {
+    public Person findById(@PathVariable(value = "id") Long id) {
         return services.findeById(id);
     }
 
@@ -45,7 +45,7 @@ public class PersonController {
 
     @RequestMapping(value = "/{id}",
             method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") String id) {
+    public void delete(@PathVariable(value = "id") Long id) {
         services.delete(id);
     }
 }
