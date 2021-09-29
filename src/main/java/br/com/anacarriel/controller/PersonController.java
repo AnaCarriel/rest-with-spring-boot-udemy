6d30f659628b1133fb41ef5c696bd499e63df975
AnaCarriel <ana.carriel096@gmail.com>
2021-09-28 21:38:45 -0300

    Ajustado o person controller

--- src/main/java/br/com/anacarriel/controller/PersonController.java
+++ src/main/java/br/com/anacarriel/controller/PersonController.java
@@ -3,7 +3,7 @@ package br.com.anacarriel.controller;
 import br.com.anacarriel.model.Person;
 import br.com.anacarriel.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,36 +16,29 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}")
     public Person findById(@PathVariable(value = "id") Long id) {
         return services.findeById(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<Person> findAll () {
         return services.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public Person create (@RequestBody Person person) {
         return services.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping
     public Person update (@RequestBody Person person) {
         return services.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         services.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
